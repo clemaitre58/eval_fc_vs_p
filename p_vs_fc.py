90ca2a46957dc1648fa747c4c251ca18fff566e4
Cedric Lemaitre <c.lemaitre58@gmail.com>
2020-04-03 19:09:09 +0200

    crop ok

--- p_vs_fc.py
+++ p_vs_fc.py
@@ -2,15 +2,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename_fit = '/Volumes/HD/Apeira/Ergocycle/Physio/Data/TestPma/2020-04-02-15-10-05.csv'
+def crop_data(data, start, end):
+	return data[start, end]
 
-df = pd.read_csv(filename_fit, sep=';')
-P = df['Power'].tolist()
-Fc = df['Fc'].tolist()
+if name == '__name__' :
 
-x = np.linspace(0, len(P), num=len(P))
+	filename_fit = '/Volumes/HD/Apeira/Ergocycle/Physio/Data/TestPma/2020-04-02-15-10-05.csv'
+	start = 0
+	end = 1440
+	df = pd.read_csv(filename_fit, sep=';')
+	P = df['Power'].tolist()
+	Fc = df['Fc'].tolist()
 
-plt.figure()
-plt.plot(x, P)
-plt.plot(x, Fc)
-plt.show()
+	P_c = crop_data(P, start, end)
+
+
+	x = np.linspace(0, len(P), num=len(P))
+
+	plt.figure()
+	plt.plot(x, P)
+	plt.plot(x, Fc)
+	plt.show()

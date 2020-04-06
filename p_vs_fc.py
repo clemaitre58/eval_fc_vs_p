54369c13f5ac71e90536f163af89862e5116ec97
Cedric Lemaitre <c.lemaitre58@gmail.com>
2020-04-06 14:23:43 +0200

    calcul derive moyenne ok

--- p_vs_fc.py
+++ p_vs_fc.py
@@ -19,6 +19,7 @@ class data_P_Fc:
 		self.std_Fc = 0
 		self.nb_data = 0
 		self.derive_seg = 0
+		self.param_fit = []
 		self.process_data()
 
 	def process_data(self):
@@ -44,7 +45,7 @@ def extract_signal(data_p, data_fc, time_step=180, time_interest=90):
 
 	return l_roi_step
 
-def display(l_data):
+def print_stat_step(l_data):
 	print('step.mean_P' + '|\t' + 'step.mean_Fc' + '|\t' + 'step.std_P' + '|\t' + 'step.std_Fc')
 	for step in l_data:
 		print("%.2f" % step.mean_P + '|\t' + "%.2f" % step.mean_Fc + '|\t' + "%.2f" % step.std_P + '|\t' + "%.2f" % step.std_Fc)
@@ -66,17 +67,46 @@ def create_array_debug(l_step, siz):
 def func_aff(x, a, b):
 	return a * x + b
 
-def computer_derive(step):
+def compute_derive(step):
 	x = np.linspace(0, step.nb_data, num=step.nb_data)
 	y = step.data_Fc
 	popt, pcov = curve_fit(func_aff, x, y)
-	plt.plot(x, y, 'b-', label='data')
-	plt.plot(x, func_aff(x, *popt), 'r-', label='fit: a=%5.3f, b=%5.3f' % tuple(popt))
-	plt.xlabel('x')
-	plt.ylabel('y')
+
+	step.param_fit = popt[:]
+
+def computer_derive_all(l_step):
+	for step in l_step:
+		compute_derive(step)
+
+def plot_all_derive(l_step):
+	cpt = 0
+	fig, axs = plt.subplots(len(l_step), 1, constrained_layout=True)
+	for step in l_step:
+		x = np.linspace(0, step.nb_data, num=step.nb_data)
+		y = step.data_Fc
+
+		axs[cpt].plot(x, y, 'o')
+		axs[cpt].plot(x, func_aff(x, *step.param_fit), 'r-')
+		# axs[cpt].set_title('Palier : ' + str(cpt))
+		# axs[cpt].set_xlabel('Temps de le palier (s)')
+		# axs[cpt].set_ylabel('Fc')
+		cpt += 1
 	plt.legend()
 	plt.show()
 
+def print_derive(l_step):
+	print('Valeur derive')
+	for step in l_step:
+		print("%.4f" % step.param_fit[0])
+
+def print_derive_moyenne(l_step):
+	print('Valeur derive moyenne (augmenation par minute)')
+	l_derive = []
+	for step in l_step:
+		l_derive.append(step.param_fit[0])
+
+	derive_moy = np.mean(l_derive) * 60
+	print("%.4f" % derive_moy)
 
 
 if __name__ == '__main__':
@@ -95,9 +125,12 @@ if __name__ == '__main__':
 
 	l_data = extract_signal(P_c, Fc_c, 180, 90)
 	debug_data = create_array_debug(l_data, len(P_c))
-	display(l_data)
+	print_stat_step(l_data)
 
-	computer_derive(l_data[4])
+	computer_derive_all(l_data)
+	print_derive(l_data)
+	print_derive_moyenne(l_data)
+	plot_all_derive(l_data)
 
 
 	x = np.linspace(0, len(P_c), num=len(P_c))

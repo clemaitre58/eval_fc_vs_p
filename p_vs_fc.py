7aa3883fe5d177d3b43fd23a6d3924a7fd3af8ec
Cedric Lemaitre <c.lemaitre58@gmail.com>
2020-04-06 11:08:36 +0200

    add debug and check metrics en segment

--- p_vs_fc.py
+++ p_vs_fc.py
@@ -7,8 +7,8 @@ class data_P_Fc:
 	def __init__(self, data_P, data_Fc, start, end):
 		self.data_P = deepcopy(data_P)
 		self.data_Fc = deepcopy(data_Fc)
-		self.start = start
-		self.end = end
+		self.start = int(start)
+		self.end = int(end)
 		self.mean_P = 0
 		self.std_P = 0
 		self.mean_Fc = 0
@@ -32,19 +32,35 @@ def crop_data(data, start, end):
 def extract_signal(data_p, data_fc, time_step=180, time_interest=90):
 	nb_step = len(data_p) // time_step
 	l_roi_step = []
-	print("size data : ", len(data_p))
-	print("nb_step : ", nb_step)
+	# print("size data : ", len(data_p))
+	# print("nb_step : ", nb_step)
 	for i in range(nb_step):
 		start = i * time_step + time_interest
-		end = start + time_step
+		end = i * time_step + time_step
+		# print("start : " + str(start) + " end : " + str(end))
 		l_roi_step.append(data_P_Fc(data_p[start:end], data_fc[start:end], start, end))
 
 	return l_roi_step
 
 def display(l_data):
-	print('step.mean_P' + '|\t' + 'step.mean_Fc' + '|\t' + 'step.mean_P' + '|\t' + 'step.mean_Fc')
+	print('step.mean_P' + '|\t' + 'step.mean_Fc' + '|\t' + 'step.std_P' + '|\t' + 'step.std_Fc')
 	for step in l_data:
 		print("%.2f" % step.mean_P + '|\t' + "%.2f" % step.mean_Fc + '|\t' + "%.2f" % step.std_P + '|\t' + "%.2f" % step.std_Fc)
+
+def create_array_debug(l_step, siz):
+	displ_data = np.zeros(siz)
+	for step in l_step:
+		# print(step.start)
+		# print(step.end)
+		# print(step.end-step.start)
+		idx = np.linspace(step.start, step.end, num=(step.end-step.start))
+		for i in idx:
+			# print(int(i))
+			if i < siz:
+				displ_data[int(i)] = 400
+
+	return displ_data
+
 
 if __name__ == '__main__':
 
@@ -61,13 +77,15 @@ if __name__ == '__main__':
 	Fc_c = crop_data(Fc, start, end)
 
 	l_data = extract_signal(P_c, Fc_c, 180, 90)
+	debug_data = create_array_debug(l_data, len(P_c))
 	display(l_data)
 
 	# TODO : display all start and stop sur le graphique
 
 	x = np.linspace(0, len(P_c), num=len(P_c))
 
-	# plt.figure()
-	# plt.plot(x, P_c)
-	# plt.plot(x, Fc_c)
-	# plt.show()
+	plt.figure()
+	plt.plot(x, P_c)
+	plt.plot(x, Fc_c)
+	plt.plot(x, debug_data)
+	plt.show()
